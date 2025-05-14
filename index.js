3fdfdc05673a2d8d1cd76da8a489cf6e57abbfb9
Prachi Heda <prachiheda@gmail.com>
2025-05-13 17:27:01 -0700

    removed backend stuff

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
     async function updateCounter() {
         try {
-            let response = await fetch("https://5s7pxnrz3q7gt2uh7bdvfn22p40gmatb.lambda-url.us-east-2.on.aws/"); // ensure correct URL format
+            let response = await fetch("https://31tioiek50.execute-api.us-east-2.amazonaws.com/"); // ensure correct URL format
             let data = await response.json();  // call the function, not just reference it
-            counter.innerHTML = `${data}`;
+            counter.innerHTML = `${data.count}`;
         } catch (error) {
             console.error("Error fetching counter data:", error);
         }
